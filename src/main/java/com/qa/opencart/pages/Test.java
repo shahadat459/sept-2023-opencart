33dced92a47ba1f63b718cb6ac831325ff1a92f2
shahadat khan <khanshahadat459@gmail.com>
2023-09-11 16:07:40 +0530

    testing-1

--- src/main/java/com/qa/opencart/pages/Test.java
+++ src/main/java/com/qa/opencart/pages/Test.java
@@ -1,9 +1,15 @@
 package com.qa.opencart.pages;
 
-public class Test {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Test {
+	
 	public static void main(String[] args) {
 		System.out.println("Allahu Akbar...");
+		WebDriver driver = new ChromeDriver();
+		//pull request test - 1
 
 	}
 
